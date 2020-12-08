bdc6413f4763535b16659e1934cd49d32178ac91
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-12-08 18:25:32 +0900

    add create action

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -3,5 +3,21 @@ class DonationsController < ApplicationController
   end
 
   def new
+    @user = User.new(donation_params)
+  end
+
+  def create
+    @user = User.new(donation_params)
+    if @user.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def donation_params
+    params.require(:user).permit(:name, :name_reading, :nickname)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "donations/index"
   root "donations#index"
-  resources :donations, only: [:index, :new]
+  resources :donations, only: [:index, :new, :create]
 end
