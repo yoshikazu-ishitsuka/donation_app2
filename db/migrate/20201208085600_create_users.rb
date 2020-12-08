45fd545719daafd46a9da6d16b9acf4e06369f54
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-12-08 18:21:12 +0900

    run db:migrate

--- db/migrate/20201208085600_create_users.rb
+++ db/migrate/20201208085600_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-
+      t.string :name, null: false
+      t.string :name_reading, null: false
+      t.string :nickname, null: false
       t.timestamps
     end
   end
