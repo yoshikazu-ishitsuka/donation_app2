e2db78ee391dc1b82a9cd7d8abfe048737efc708
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-12-08 18:17:35 +0900

    update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_one :address
+  has_one :donation
+  with_options presence: true do
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
+    validates :name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
+    validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters." }
+  end
 end
